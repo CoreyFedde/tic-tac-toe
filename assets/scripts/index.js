442260d91f8356f06b2b2dbc815723cc96cfc411
coreyfedde <coreysfedde@gmail.com>
2017-06-27 13:21:49 -0400

    Create box for game text
    Created a box for text that shows which player's move it is. Still
    working on how to alternate players... possibly creating a variable to
    hold the last move and then using an if else to compare the content and
    direct who plays.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,6 +13,7 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 require('./example')
 
+// Changes box to show X
 $('#game-board').on('click', function (event) {
   const value = $(event.target).text()
   console.log(value)
@@ -20,5 +21,18 @@ $('#game-board').on('click', function (event) {
   // console.log('this is the event.target: ', event.target)
   if (value === '') {
     $(event.target).text('X')
-  }
+    $('#game-text').text('Your move Player 2')
+  } else ($('#game-text').text('INVALID MOVE MORON'))
+})
+
+// Changes box to show O
+$('#game-board').on('click', function (event) {
+  const value = $(event.target).text()
+  console.log(value)
+  // console.log('this is the event: ', event)
+  // console.log('this is the event.target: ', event.target)
+  if (value === '') {
+    $(event.target).text('O')
+    $('#game-text').text('Your move Player 1')
+  } else ($('#game-text').text('INVALID MOVE... and you looked so competent before'))
 })
