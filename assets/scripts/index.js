503c99ed7775a3bc0874a9a2a019676cd9dbbf12
coreyfedde <coreysfedde@gmail.com>
2017-06-27 13:08:44 -0400

    Add function to change value to X
    Added a function and styling so that when a box in the gameboard is
    clicked, the value becomes 'X'.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,13 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+$('#game-board').on('click', function (event) {
+  const value = $(event.target).text()
+  console.log(value)
+  // console.log('this is the event: ', event)
+  // console.log('this is the event.target: ', event.target)
+  if (value === '') {
+    $(event.target).text('X')
+  }
+})
