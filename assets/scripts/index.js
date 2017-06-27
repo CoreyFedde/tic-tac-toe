976079b722d1caf8b46a8ee0cab2c6988717b289
coreyfedde <coreysfedde@gmail.com>
2017-06-27 14:20:43 -0400

    Fixed game log | add button
    Fixed an issue with the game log where it was getting stuck on Player 1.
    Now the first if statement is centered around Player 2 and it is fixed.
    
    Also added button and am still working on setting a new game with it.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,38 +13,46 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 require('./example')
 
-const win = ($('#box-1').val() === $('#box-2').val() === $('#box-3').val()) ||
-($('#box-1').val() === $('#box-4').val() === $('#box-7').val()) ||
-($('#box-1').val() === $('#box-5').val() === $('#box-9').val()) ||
-($('#box-3').val() === $('#box-5').val() === $('#box-7').val()) ||
-($('#box-3').val() === $('#box-6').val() === $('#box-9').val()) ||
-($('#box-7').val() === $('#box-8').val() === $('#box-9').val()) ||
-($('#box-2').val() === $('#box-5').val() === $('#box-8').val())
+// Win conditions and possible way to compare, but not working
+// const win = ($('#box-1').val() === $('#box-2').val() === $('#box-3').val()) ||
+// ($('#box-1').val() === $('#box-4').val() === $('#box-7').val()) ||
+// ($('#box-1').val() === $('#box-5').val() === $('#box-9').val()) ||
+// ($('#box-3').val() === $('#box-5').val() === $('#box-7').val()) ||
+// ($('#box-3').val() === $('#box-6').val() === $('#box-9').val()) ||
+// ($('#box-7').val() === $('#box-8').val() === $('#box-9').val()) ||
+// ($('#box-2').val() === $('#box-5').val() === $('#box-8').val())
+// if (win === true) {
+//   console.log('Game Over')
+// }
 
+// Below is the logic for the game-board, which is also linked to the game-text
 $('#game-board').on('click', function (event) {
   // Want a variable that capture the value of game-text before each move
-  const lastTurn = $('#game-text').text()
+  let lastTurn = $('#game-text').text()
   console.log(lastTurn)
   // Changes box to show X
-  if (lastTurn === 'Your move Player 1' || '') {
+  if (lastTurn === 'Your move Player 2') {
+    // Changes box to show O
+        // $('#game-board').on('click', function (event) {
+    const value = $(event.target).text()
+    if (value === '') {
+      $(event.target).text('O')
+      $('#game-text').text('Your move Player 1')
+          // } else ($('#game-text').text('INVALID MOVE... and you looked so competent before'))
+        }
+  } else {
     const value = $(event.target).text()
     if (value === '') {
       $(event.target).text('X')
       $('#game-text').text('Your move Player 2')
     // } else ($('#game-text').text('INVALID MOVE MORON'))
-    }
-  } else {
-// Changes box to show O
-    // $('#game-board').on('click', function (event) {
-    const value = $(event.target).text()
-    if (value === '') {
-      $(event.target).text('O')
-      $('#game-text').text('Your move Player 1')
-      // } else ($('#game-text').text('INVALID MOVE... and you looked so competent before'))
+      console.log(lastTurn)
     }
   }
 })
 
-if (win === true) {
-  console.log('Game Over')
-}
+// New Game button, which should reset all squares to have a value of "''" and allow the users to start playing
+$('#new-game-button').on('click', function (event) {
+  console.log('button clicked')
+  $('#game-text').text('New game!')
+})
