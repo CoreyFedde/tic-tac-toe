4415126549e0a6a7a4ef70801783d5b4c541286a
coreyfedde <coreysfedde@gmail.com>
2017-06-27 18:34:21 -0400

    Trouble with win calculator
    I have added a turn calculator that I have used to base the time to
    check for a win or game over. Only partially finished, but having trouble.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,6 +14,18 @@ $(() => {
 require('./example')
 
 // Win conditions and possible way to compare, but not working
+
+// const win = [
+//   ($('#box-1').val() === $('#box-2').val() === $('#box-3').val()),
+//   ($('#box-1').val() === $('#box-4').val() === $('#box-7').val()),
+//   ($('#box-3').val() === $('#box-5').val() === $('#box-7').val()),
+//   ($('#box-4').val() === $('#box-5').val() === $('#box-6').val()),
+//   ($('#box-3').val() === $('#box-5').val() === $('#box-7').val()),
+//   ($('#box-3').val() === $('#box-6').val() === $('#box-9').val()),
+//   ($('#box-7').val() === $('#box-8').val() === $('#box-9').val()),
+//   ($('#box-2').val() === $('#box-5').val() === $('#box-8').val())
+//   ($('#box-1').val() === $('#box-5').val() === $('#box-9').val())
+// ]
 // const win = ($('#box-1').val() === $('#box-2').val() === $('#box-3').val()) ||
 // ($('#box-1').val() === $('#box-4').val() === $('#box-7').val()) ||
 // ($('#box-1').val() === $('#box-5').val() === $('#box-9').val()) ||
@@ -22,8 +34,70 @@ require('./example')
 // ($('#box-7').val() === $('#box-8').val() === $('#box-9').val()) ||
 // ($('#box-2').val() === $('#box-5').val() === $('#box-8').val())
 // if (win === true) {
-//   console.log('Game Over')
-// }
+// //   console.log('Game Over')
+// // }
+
+let turnCounter = 0
+
+$('#check-win').on('click', function () {
+  console.log('clicked')
+  console.log(turnCounter)
+  let boxOne = $('#box-1').text()
+  console.log(boxOne)
+  let boxTwo = $('#box-2').text()
+  console.log(boxTwo)
+  let boxThree = $('#box-3').text()
+  console.log(boxThree)
+  let boxFour = $('#box-4').text()
+  console.log(boxFour)
+  let boxFive = $('#box-5').text()
+  console.log(boxFive)
+  let boxSix = $('#box-6').text()
+  console.log(boxSix)
+  let boxSeven = $('#box-7').text()
+  console.log(boxSeven)
+  let boxEight = $('#box-8').text()
+  console.log(boxEight)
+  let boxNine = $('#box-9').text()
+  console.log(boxNine)
+  // Horizontal wins
+  if (turnCounter > 3) {
+    console.log('hor 1 works')
+    console.log('this is boxOne: ', boxOne)
+    console.log('this is boxTwo: ', boxTwo)
+    console.log('this is boxthree: ', boxThree)
+    if (boxOne === boxTwo === boxThree) {
+      console.log('Game over becuase of first line')
+    }
+    if (boxFour === boxFive === boxSix) {
+      console.log('Game over because of second line!')
+    }
+    if (boxSeven === boxEight === boxNine) {
+      console.log('Game over because of third line!')
+    }
+      // Vertical wins
+    if (boxOne === boxFour === boxSeven) {
+      console.log('Game over of fourth line!')
+    }
+    if (boxTwo === boxFive === boxEight) {
+      console.log('Game over of fifth line!')
+    }
+    if (boxThree === boxSix === boxNine) {
+      console.log('Game over of sixth line!')
+    }
+    // Diagnol wins
+    if (boxOne === boxFive === boxNine) {
+      console.log('Game over of seventh line!')
+    }
+    if (boxThree === boxFive === boxSeven) {
+      console.log('Game over of eight line!')
+    }
+  }
+  // Maximum amount of moves
+  if (turnCounter === 9) {
+    console.log('Game over 9!')
+  }
+})
 
 // Below is the logic for the game-board, which is also linked to the game-text
 $('#game-board').on('click', function (event) {
@@ -38,6 +112,8 @@ $('#game-board').on('click', function (event) {
     if (value === '') {
       $(event.target).text('O')
       $('#game-text').text('Your move Player 1')
+      turnCounter++
+      console.log(turnCounter)
           // } else ($('#game-text').text('INVALID MOVE... and you looked so competent before'))
     }
   } else {
@@ -45,6 +121,8 @@ $('#game-board').on('click', function (event) {
     if (value === '') {
       $(event.target).text('X')
       $('#game-text').text('Your move Player 2')
+      turnCounter++
+      console.log(turnCounter)
     // } else ($('#game-text').text('INVALID MOVE MORON'))
     }
   }
@@ -54,8 +132,17 @@ $('#game-board').on('click', function (event) {
 $('#new-game-button').on('click', function (event) {
   console.log('button clicked')
   $('#game-text').text('New game!')
+  turnCounter = 0
   // Create an array to represent the game-board
   $('.game').each(function () {
     $(this).text('')
   })
 })
+
+// Turn counter. If turns = 9 then the game is over. If turns > 3, check to see
+// if anyone has won.
+// $('.game').each(function () {
+//   if ($(this).text('') !== true) {
+//     console.log('Game is over')
+//   }
+// })
