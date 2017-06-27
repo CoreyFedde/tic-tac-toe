e58d6843c964f3b232cb46642c7d6453613bdc97
coreyfedde <coreysfedde@gmail.com>
2017-06-27 13:40:31 -0400

    Add game logic for alternating Xs and Os
    Added logic for the game that alternates between adding Xs or Ox based
    on the value of the game-text. There is probably a better way to do this
    but for now it works.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,26 +13,26 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 require('./example')
 
-// Changes box to show X
 $('#game-board').on('click', function (event) {
-  const value = $(event.target).text()
-  console.log(value)
-  // console.log('this is the event: ', event)
-  // console.log('this is the event.target: ', event.target)
-  if (value === '') {
-    $(event.target).text('X')
-    $('#game-text').text('Your move Player 2')
-  } else ($('#game-text').text('INVALID MOVE MORON'))
-})
-
+  // Want a variable that capture the value of game-text before each move
+  let lastTurn = $('#game-text').text()
+  console.log(lastTurn)
+  // Changes box to show X
+  if (lastTurn === 'Your move Player 1' || '') {
+    const value = $(event.target).text()
+    if (value === '') {
+      $(event.target).text('X')
+      $('#game-text').text('Your move Player 2')
+    // } else ($('#game-text').text('INVALID MOVE MORON'))
+    }
+  } else {
 // Changes box to show O
-$('#game-board').on('click', function (event) {
-  const value = $(event.target).text()
-  console.log(value)
-  // console.log('this is the event: ', event)
-  // console.log('this is the event.target: ', event.target)
-  if (value === '') {
-    $(event.target).text('O')
-    $('#game-text').text('Your move Player 1')
-  } else ($('#game-text').text('INVALID MOVE... and you looked so competent before'))
+    // $('#game-board').on('click', function (event) {
+    const value = $(event.target).text()
+    if (value === '') {
+      $(event.target).text('O')
+      $('#game-text').text('Your move Player 1')
+      // } else ($('#game-text').text('INVALID MOVE... and you looked so competent before'))
+    }
+  }
 })
